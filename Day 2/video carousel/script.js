53848fe5ebf2ba6567ffb8f4f2c44fc46f803d37
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2021-11-03 21:38:49 +0530

    update day 2

---
+++ Day 2/video carousel/script.js
@@ -0,0 +1,80 @@
+let slideIndex = 0;
+let slider = document.querySelector(".slider");
+let slides = document.querySelector(".slides");
+let slide = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dots span")
+
+showslide();
+
+function plusslide(position) {
+    slideIndex += position;
+
+    if (slideIndex > slide.length) {
+        slideIndex = 1;
+    }
+    else if (slideIndex < 1) {
+        slideIndex = slide.length;
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        const element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${slideIndex - 1}00%`;
+    dots[slideIndex - 1].classList.add("dot-active");
+}
+
+function currentslide(index) {
+    if (index > slide.length) {
+        index = 1;
+    }
+    else if (index < 1) {
+        index = slide.length;
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        const element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${index - 1}00%`;
+    dots[index - 1].classList.add("dot-active");
+
+    slideIndex = index;
+}
+
+function showslide() {
+    slideIndex++;
+
+    if (slideIndex > slide.length) {
+        slideIndex = 1;
+    }
+    else if (slideIndex < 1) {
+        slideIndex = slide.length;
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        const element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${slideIndex - 1}00%`;
+    dots[slideIndex - 1].classList.add("dot-active");
+}
+
+
+let interval = setInterval(()=> {
+    showslide();
+} , 3000);   
+
+slider.addEventListener("mouseover" , ()=> {
+    clearInterval(interval);     
+});
+
+slider.addEventListener("mouseout" , ()=> {
+    interval = setInterval(()=> {
+        showslide();   
+    } , 3000);    
+});
+
